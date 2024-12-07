cae53b84531fbe4b309cfcde475cb448806da26f
Aru Poleo Vargas <arupoleo@gmail.com>
2024-12-07 07:43:46 -0500

    first commit

---
+++ src/src/main.py
@@ -0,0 +1,20 @@
+import csv
+
+def compare_files(file1_path: str, file2_path: str):
+
+    # Conseguir los iguales
+
+    # Abrir ambos archivos
+    # Iteramos sobre las lineas de un archivo
+        # Iteramos sobre las lineas del otro archivo
+            # Si el log es el mismo, lo agregamos
+            # Si el log no es el mismo, pasamos
+
+    pass
+
+def compare_logs(log1: str, log2: str):
+
+    pass
+
+if __name__ == "__main__":
+    pass
